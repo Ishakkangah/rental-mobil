e7ff8b71c5218fac118702bda52631c4b053febe
Ishak <ishakkangah@gmail.com>
2023-02-19 21:56:08 +0700

    Menambahkan menu detail mobil

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -40,14 +40,71 @@
                     <td class="align-middle">{{ $car->price }}</td>
                     <td class="align-middle">
                         <div class="btn-group">
-                            <button class="btn btn-sm btn-info rounded shadow-sm  mx-1">
-                                <a href="{{ route('car.show', $car->id) }}" class="text-white text-decoration-none">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                        fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
-                                        <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-                                        <path
-                                            d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-                                    </svg></a></button>
+                            <button class="btn btn-sm btn-info rounded shadow-sm text-white text-decoration-none mx-1"
+                                data-bs-toggle="modal" data-bs-target="#detail{{ $car->id }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                    <path
+                                        d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                </svg></button>
+
+
+                            <!-- MODAL DETAIL MOBIL -->
+                            <div class="modal fade" id="detail{{ $car->id }}" tabindex="-1"
+                                aria-labelledby="detail{{ $car->id }}label" data-bs-backdrop="static"
+                                data-bs-keyboard="false" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">DETAIL MOBIL</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <table class="table table-borderless">
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Nama</td>
+                                                        <td>{{ $car->name }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Plat</td>
+                                                        <td>{{ $car->plat }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Deskripsi</td>
+                                                        <td>{{ $car->description }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Harga</td>
+                                                        <td>Rp. {{ number_format($car->price, 2) }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Status</td>
+                                                        @if ($car->status == 1)
+                                                            <td> <span class="badge bg-danger">Available</span></td>
+                                                        @else
+                                                            <td> <span class="badge bg-danger">Not Availables</span></td>
+                                                        @endif
+                                                    </tr>
+                                                    <tr>
+                                                        <td><img src="{{ asset('storage/' . $car->image) }}"
+                                                                class="card-img-top" alt="image"></td>
+                                                    </tr>
+
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
                             <button class="btn btn-sm btn-primary  rounded shadow-sm mx-1">
                                 <a href="{{ route('car.edit', $car->id) }}" class="text-white text-decoration-none">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
@@ -69,6 +126,7 @@
                                     </svg>
                                 </button>
                             </form>
+                        </div>
                         </div>
                     </td>
                 </tr>
